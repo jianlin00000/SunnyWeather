ea1196ecac5bcd19159a06f5f1e0f1971b0a96c5
jianlinchen <524860983@qq.com>
2021-01-27 09:36:51 +0800

    完成切换城市的功能

--- app/src/main/java/cjl/com/sunnyweather/ui/weather/WeatherActivity.kt
+++ app/src/main/java/cjl/com/sunnyweather/ui/weather/WeatherActivity.kt
@@ -1,14 +1,19 @@
 package cjl.com.sunnyweather.ui.weather
 
+import android.content.Context
 import android.graphics.Color
+import android.hardware.input.InputManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -38,6 +43,24 @@ class WeatherActivity : AppCompatActivity() {
         }
         
         initData()
+
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        drawerLayout.addDrawerListener(object:DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerOpened(drawerView: View) {}
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+
+        })
     }
 
     private fun initData() {
@@ -67,7 +90,7 @@ class WeatherActivity : AppCompatActivity() {
         }
     }
 
-    private fun refreshWeather() {
+    fun refreshWeather() {
         viewModel.refreshWeather(viewModel.locationLng, viewModel.locationLat)
         swipeRefresh.isRefreshing=true
     }
